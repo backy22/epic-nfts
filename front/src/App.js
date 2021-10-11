6475a88e62af53ccc0a39f7c6e71bb1d1132d30f
Aya Tsubakino <happy.backy@gmail.com>
2021-10-11 00:32:06 -0400

    move function inside useeffect

--- front/src/App.js
+++ front/src/App.js
@@ -20,31 +20,65 @@ const App = () => {
   const [minting, setMinting] = useState(false)
 
   useEffect(() => {
+
+    const checkIfWalletIsConnected = async () => {
+      const { ethereum } = window;
+  
+      if (!ethereum) {
+          console.log("Make sure you have metamask!");
+          return;
+      } else {
+          console.log("We have the ethereum object", ethereum);
+      }
+  
+      const accounts = await ethereum.request({ method: 'eth_accounts' });
+  
+      const isTestNet = await checkChainId();
+      if (accounts.length !== 0 && isTestNet) {
+          const account = accounts[0];
+          console.log("Found an authorized account:", account);
+          setCurrentAccount(account)
+      } else {
+          console.log("No authorized account found")
+      }
+    }
+  
+      // Setup our listener.
+      const setupEventListener = async () => {
+        // Most of this looks the same as our function askContractToMintNft
+        try {
+          const { ethereum } = window;
+    
+          if (ethereum) {
+            // Same stuff again
+            const provider = new ethers.providers.Web3Provider(ethereum);
+            const signer = provider.getSigner();
+            const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+    
+            // THIS IS THE MAGIC SAUCE.
+            // This will essentially "capture" our event when our contract throws it.
+            // If you're familiar with webhooks, it's very similar to that!
+            connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
+              console.log(from, tokenId.toNumber())
+              setOpenseaLink(OPENSEA_LINK + tokenId.toNumber());
+              //alert(`Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`)
+            });
+    
+            await getTotalNFTsMintedSoFar(connectedContract);
+    
+            console.log("Setup event listener!")
+    
+          } else {
+            console.log("Ethereum object doesn't exist!");
+          }
+        } catch (error) {
+          console.log(error)
+        }
+      }
+
     checkIfWalletIsConnected();
     setupEventListener();
   }, [])
-    
-  const checkIfWalletIsConnected = async () => {
-    const { ethereum } = window;
-
-    if (!ethereum) {
-        console.log("Make sure you have metamask!");
-        return;
-    } else {
-        console.log("We have the ethereum object", ethereum);
-    }
-
-    const accounts = await ethereum.request({ method: 'eth_accounts' });
-
-    const isTestNet = await checkChainId();
-    if (accounts.length !== 0 && isTestNet) {
-        const account = accounts[0];
-        console.log("Found an authorized account:", account);
-        setCurrentAccount(account)
-    } else {
-        console.log("No authorized account found")
-    }
-  }
 
   const checkChainId = async() => {
     const { ethereum } = window;
@@ -86,39 +120,6 @@ const App = () => {
         setCurrentAccount(accounts[0]); 
       } else {
         alert("You are not connected to the Rinkeby Test Network!");
-      }
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  // Setup our listener.
-  const setupEventListener = async () => {
-    // Most of this looks the same as our function askContractToMintNft
-    try {
-      const { ethereum } = window;
-
-      if (ethereum) {
-        // Same stuff again
-        const provider = new ethers.providers.Web3Provider(ethereum);
-        const signer = provider.getSigner();
-        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
-
-        // THIS IS THE MAGIC SAUCE.
-        // This will essentially "capture" our event when our contract throws it.
-        // If you're familiar with webhooks, it's very similar to that!
-        connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
-          console.log(from, tokenId.toNumber())
-          setOpenseaLink(OPENSEA_LINK + tokenId.toNumber());
-          //alert(`Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`)
-        });
-
-        await getTotalNFTsMintedSoFar(connectedContract);
-
-        console.log("Setup event listener!")
-
-      } else {
-        console.log("Ethereum object doesn't exist!");
       }
     } catch (error) {
       console.log(error)
